f860acb500b4457c339e11f3519e2936c8a41d14
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-08 19:04:58 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solutions in this file
+/*Code solutions here
+for (let age = 30; age < 40; age ++){
+    console.log(`I/'m ${age} years old. Happy birthday to me!`);
+     
+} */
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`); 
+  }
+  return gifts;
+}
+wrapGifts(gifts); */
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length ; i++) {
+     console.log(messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`));
+    debugger;
+    }
+    return messages;
+  } 
+  writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+/*let countDown = 11;
+while (countDown > 0){
+    countDown-- 
+console.log(countDown)
+} */
+
+function countDown(){
+    let countDown = 11;
+    let i = 0;
+    while(countDown > i) {
+        countDown --;
+        console.log(countDown);
+    }
+}
